8681aee117392ce9c1bde1bc8e033989d5812f40
Abhishek C. Sharma <portgasd666@gmail.com>
2017-11-12 10:39:02 +0530

    - Added status fields to Orders Collection : -1 = Payment Pending, 0 = Placed, 1 = Serviced, 2 = Archived - Added various status timing fileds to Orders : placedTime, servicedTime and archivedTime - Added PUT route to order API to update status and appropriate time - Added Service Order button to the Order Processing page - Fixed order being sent to Order Processing page before Payment

--- routes/api/order.js
+++ routes/api/order.js
@@ -9,7 +9,18 @@ module.exports = function(db, wss) {
 
   // get all orders
   router.get('/', function(req, res) {
-    ordersCollection.find({}).toArray(function(err, orders) {
+		let cursor;
+		req.query.status = parseInt(req.query.status, 10);
+		if(req.query.status === 0) {
+			cursor = ordersCollection.find({status: 0});
+		} else if(req.query.status === 1) {
+			cursor = ordersCollection.find({status: 1}).sort({servicedTime: -1});
+		} else if(req.query.status === 2) {
+			cursor = ordersCollection.find({status: 2});
+		} else {
+			cursor = ordersCollection.find({});
+		}
+    cursor.toArray(function(err, orders) {
       if(err) {
         res.json({'error': 'Unable to fetch orders'});
       }
@@ -19,14 +30,36 @@ module.exports = function(db, wss) {
 
   // create a new order
   router.post('/', function(req, res) {
-    ordersCollection.insert({items: req.body.items}, function(err, result) {
+    ordersCollection.insert({items: req.body.items, status: -1}, function(err, result) {
       if(err) {
         res.json({'error': 'Unable to insert order'});
       }
-      wss.broadcast(JSON.stringify({'newOrder': {id: result.insertedIds[0], items: req.body.items}}));
       res.json({'success': result.insertedIds[0]});
     });
   });
+
+	router.put('/:id', function(req, res) {
+		let update;
+		if(parseInt(req.body.status, 10) === 0) {
+			update = {$set: {status : 0, placedTime: Date.now()}};
+		} else if(parseInt(req.body.status, 10) === 1) {
+			update = {$set: {status : 1, servicedTime: Date.now()}};
+		} else if(parseInt(req.body.status, 10) === 2) {
+			update = {$set: {status : 2, archivedTime: Date.now()}};
+		}
+		ordersCollection.updateOne({_id: ObjectID(req.params.id)}, update, function(err, order) {
+			if(err) {
+				res.json({'error': `Unable to change status of order ${req.params.id} to ${req.body.status}`});
+			}
+
+			if(parseInt(req.body.status) === 0) {
+				wss.broadcast(JSON.stringify({'placedOrder': {id: req.params.id, items: req.body.items}}));
+			} else if(parseInt(req.body.status) === 1) {
+				wss.broadcast(JSON.stringify({'servicedOrder': req.params.id}));
+			}
+			res.json({'success': `Order ${req.params.id} to Status ${req.body.status}`});
+		});
+	});
 
   router.delete('/:id', function(req, res) {
     ordersCollection.deleteOne({_id: ObjectID(req.params.id)}, function(err, order) {
